e46f4f12ef5e9a3e2624f05e02043e2ab4cd8ae9
unknown <thiagorizzo@gmail.com>
2019-01-19 09:47:29 -0200

    fixed

--- src/java/controllers/ProdutoController.java
+++ src/java/controllers/ProdutoController.java
@@ -1,14 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package controllers;
 
 import controllers.interfaces.IProdutoService;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
 import java.util.List;
 import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,40 +18,20 @@ import javax.servlet.http.HttpServletResponse;
 import models.Produto;
 import services.ProdutoService;
 
-@WebServlet(
-    name = "Controller Produto",
-    urlPatterns = { "/Produtos", "/ListarProduto" },
-    initParams = {  
-        @WebInitParam(name = "connectionString", value = "blablabla" ),
-        @WebInitParam(name = "TituloAplicacao", value = "ProjetoWeb" ),
-    }
-)
+/**
+ *
+ * @author MAQ01LAB04
+ */
+@WebServlet(name = "ProdutoController", urlPatterns = {"/Produto"})
 public class ProdutoController extends HttpServlet {
-    
-    // TODO: injetar dependência
-    IProdutoService produtoService;
-    
-    @Override
-    public void init(ServletConfig config) {
-        String connectionString = config.getInitParameter("connectionString");
-    }
-    
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        List<Produto> produtos = produtoService.ListarProdutos();
 
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        IProdutoService produtoService = new ProdutoService();
+        List<Produto> produtos = produtoService.ListarProdutos();
+        
         request.setAttribute("produtos", produtos);
         
         RequestDispatcher rd = request.getRequestDispatcher("Produto/ListarProdutos.jsp");
         rd.forward(request, response);
-    }
-
-    @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        
-    }
-
-    @Override
-    public void destroy() {
-        
     }
 }
